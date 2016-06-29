f2d9efa11b64b49e9af883b7686ce1052fa333e0
Vicens Fayos <datracka@gmail.com>
2016-06-29 15:49:16 +0200

    delete wrong chars

--- site/web/app/themes/simplemag-child/page-colaboradores.php
+++ site/web/app/themes/simplemag-child/page-colaboradores.php
@@ -8,19 +8,14 @@
 get_header();
 global $ti_option;
 ?>
-
     <section id="content" role="main" class="clearfix anmtd">
-
         <div id="post-953" class="post-953 page type-page status-publish hentry post-item">
-
             <div class="page-title wrapper title-with-sep">
                 <header class="entry-header page-header">
                     <h1 class="entry-title">Colaboradores</h1>
                 </header>
             </div>
-
             <div class="wrapper">
-
                 <article class="entry-content">
                     <div id="x-section-1" class="x-section bg-image"
                          style=" margin: 0px; padding: 45px 0px;
@@ -46,8 +41,7 @@ global $ti_option;
                             </div>
                         </div>
                     </div>
-
-                    <div class="x-container" style="margin: 0px auto; padding: 0px; "> 
+                    <div class="x-container" style="margin: 0px auto; padding: 0px; ">
                         <div draggable="false" class="x-section " id="x-section-3"  
                              style="padding: 45px 0px;margin: 0px;">
                             <?php
@@ -64,18 +58,15 @@ global $ti_option;
                                             echo '<div draggable="false" class="x-section " id="x-section-3"  
          style="padding: 45px 0px;margin: 0px;">';
                                         }
-                                        ?>
-                                        <div class="x-column x-sm x-1-3" style="padding: 0px; ">
-                                            <div class="x-author-box cf">
-                                                <?php echo get_avatar($user->user_email) ?>
-                                                <div class="x-author-info">
-                                                    <h2 class="h-author mtn">
-                                                        <?php echo $user->first_name . " " . $user->last_name; ?>
-                                                    </h2>
-                                                    <p class="p-author mbn"><?php echo $user->description ?></p> 
-                                                </div>
+                                        ?><div class="x-column x-sm x-1-3" style="padding: 0px; "><div class="x-author-box cf">
+                                            <?php echo get_avatar($user->user_email) ?>
+                                            <div class="x-author-info">
+                                                <h2 class="h-author mtn">
+                                                    <?php echo $user->first_name . " " . $user->last_name; ?>
+                                                </h2>
+                                                <p class="p-author mbn"><?php echo $user->description ?></p>
                                             </div>
-                                             
+                                        </div>
                                         </div>
                                         <?php
                                         $index++;
@@ -86,7 +77,5 @@ global $ti_option;
                             ?>
                 </article>
             </div>
-
     </section><!-- #content -->
-
 <?php get_footer(); ?>
