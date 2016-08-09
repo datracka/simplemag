0b5f40f66591e472a90ff2f2dc480cb89d476f8e
Vicens Fayos <datracka@gmail.com>
2016-08-09 15:24:10 +0200

    last steps news

--- site/web/app/themes/simplemag-child/functions.php
+++ site/web/app/themes/simplemag-child/functions.php
@@ -3,6 +3,13 @@
 function add_custom_scripts() {
     wp_enqueue_style( 'mailchimp_css', '//cdn-images.mailchimp.com/embedcode/classic-10_7.css' );
     wp_enqueue_script(
+        'mailchimp_js',
+        '//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js',
+        'jquery',
+        '1.0',
+        true
+    );
+    wp_enqueue_script(
         'ti-custom-child',
         get_stylesheet_directory_uri() . '/js/jquery.custom.child.js',
         'jquery',
